7421424966a5604e4a6a2e9ad729f194e7f25c35
dtlucho <luciano.ditomaso@gmail.com>
2026-04-20 16:24:30 -0300

    Open update download via backend so it escapes the Wails webview

--- frontend/src/components/UpdateBanner.tsx
+++ frontend/src/components/UpdateBanner.tsx
@@ -1,4 +1,5 @@
 import type { UpdateInfo } from '../types'
+import * as api from '../api'
 import { Download } from './icons'
 
 interface UpdateBannerProps {
@@ -7,13 +8,18 @@ interface UpdateBannerProps {
 }
 
 export function UpdateBanner({ info, onDismiss }: UpdateBannerProps) {
+  const handleDownload = (e: React.MouseEvent) => {
+    e.preventDefault()
+    api.openUrl(info.download_url)
+  }
+
   return (
     <div className="update">
       <Download />
       <span className="flex-1">
         Ditto {info.latest} is available (you have {info.current}).
       </span>
-      <a href={info.download_url} target="_blank" rel="noreferrer" className="link">
+      <a href={info.download_url} onClick={handleDownload} className="link">
         Download
       </a>
       <button type="button" onClick={onDismiss} className="dismiss">
